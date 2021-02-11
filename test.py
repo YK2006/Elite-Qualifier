7ccd130bc74bb247f1bf122c5be9e6ebaf69ff8e
YusufKoroglu <yusufkerimkoroglu@gmail.com>
2021-02-11 22:29:38 +0000

    Added ci.yml

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+from main import AutoCorrect
+
+class TestIsTrue(unittest.self):
+   def self.assertTrue(is_true(seleping))
+
+   def self.assertTrue(is_false(sleeipng))
+
+if __name__ == "__main__":
+   unittest.main()
+ 
