360dfad85c2d865e238387b8a1e842791e721809
YusufKoroglu <yusufkerimkoroglu@gmail.com>
2021-02-21 17:15:56 +0000

    changed my main.py a little

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-def create_bigram(word):
-  return [word[i] + word[i+1] for i in range(len(word)-1)]
+import nltk
+from nltk.corpus import words
+from nltk.metrics.distance import (
+    edit_distance,
+    jaccard_distance,
+    )
+from nltk.util import ngrams
+nltk.download('words')
+import pandas
+correct_spellings = words.words()
+spellings_series = pandas.Series(correct_spellings)
+spellings_series
 
-def get_similarity_ratio(word1, word2):
-  word1, word2 = word1.lower(), word2.lower()
 
-  common = []
+def editreco(entries=['cormulent', 'incendenece', 'validrate']):
 
-  bigram1, bigram2 = create_bigram(word1), create_bigram(word2)
+    outcomes = []
+    for entry in entries:
+        distances = ((edit_distance(entry,
+                                    word), word)
+                     for word in correct_spellings)
+        closest = min(distances)
+        outcomes.append(closest[1])
+    return outcomes
 
-  for i in range(len(bigram1)):
-    try:
-      cmn_elt = bigram2.index(bigram1[i])
-      common.append(bigram1[i])
-    except:
-      continue
 
-  return len(common)/max(len(bigram1)), len(bigram2)
+editreco()
 
-def AutoCorrect(word, database = {'falicon', 'dragon', 'etc'}, sim_threshold=0.5):
-  max_sim = 0.0
-  most_sim_word = word
-  for data_word in database:
-    cur_sim = get_similarity_ratio(word, data_word)
-    if cur_sim > max_sim:
-      max_sim - cur_sim
-      most_sim_word = data_word
-  return most_sim_word if max_sim>sim_threshold else word
+userinput = []
+for i in range(0,3):
+    word = input("threa woeds pleese: ")
+    userinput.append(word)
+    
+userinput
+
+editreco(userinput)
